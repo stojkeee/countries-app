1f113c4495dfaebe05e144bf835f4e1b4e320a7f
stojic.mate <stojic.mate@nsoft.com>
2023-03-08 22:11:33 +0100

    add country search

--- src/app/pages/countries/countries.component.ts
+++ src/app/pages/countries/countries.component.ts
@@ -1,19 +1,48 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
-import { Observable } from 'rxjs';
+import { map, Observable, shareReplay, startWith, switchMap } from 'rxjs';
 import { Country } from 'src/app/models/country.interface';
 import { CountriesService } from 'src/app/services/countries.service';
-import { CountryCardComponent } from "../../components/country-card/country-card.component";
+import { CountryCardComponent } from '../../components/country-card/country-card.component';
 
 @Component({
-    selector: 'app-countries',
-    standalone: true,
-    templateUrl: './countries.component.html',
-    styleUrls: ['./countries.component.scss'],
-    imports: [CommonModule, MatInputModule, CountryCardComponent]
+  selector: 'app-countries',
+  standalone: true,
+  templateUrl: './countries.component.html',
+  styleUrls: ['./countries.component.scss'],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    CountryCardComponent,
+    ReactiveFormsModule,
+  ],
 })
-export class CountriesComponent {
-  countries$: Observable<Country[]> = this.countriesService.getAllCountries();
+export class CountriesComponent implements OnInit {
+  search = new FormControl('');
+  filterdCountries$: Observable<Country[]> = this.countriesService
+    .getAllCountries()
+    .pipe(shareReplay());
+  counties$: Observable<Country[]> = this.filterdCountries$;
   constructor(private countriesService: CountriesService) {}
+
+  ngOnInit(): void {
+    this.filterdCountries$ = this.search.valueChanges.pipe(
+      startWith(''),
+      switchMap((search) => {
+        if (search) {
+          return this.counties$.pipe(
+            map((countries) => {
+              return countries.filter((country) =>
+                country.name.toLowerCase().includes(search.toLowerCase())
+              );
+            })
+          );
+        } else {
+          return this.counties$;
+        }
+      })
+    );
+  }
 }
