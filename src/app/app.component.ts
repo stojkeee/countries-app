1ddca49781e31f05ea166fc366e7ce53c6f06f3d
stojic.mate <stojic.mate@nsoft.com>
2023-03-08 18:24:46 +0100

    create header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { RouterModule } from '@angular/router';
 import { Observable } from 'rxjs';
 import { LoaderService } from './services/loader.service';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterModule, MatProgressBarModule],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    imports: [CommonModule, RouterModule, MatProgressBarModule, HeaderComponent]
 })
 export class AppComponent {
   isLoading$: Observable<boolean> = this.loaderService.isLoading$;
